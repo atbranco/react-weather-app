3ee9a3d1c91da3ed4514d086735edece426b0537
atbranco <ateresabranco@gmail.com>
2023-04-17 11:16:10 +0100

    Added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import "./App.css";
+import React from "react";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://wonderful-chebakia-e5b40c.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Ana Teresa Branco
-        </a>{" "}
-        an is
-        <a
-          href="https://github.com/atbranco/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://wonderful-chebakia-e5b40c.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Ana Teresa Branco
+          </a>{" "}
+          an is{" "}
+          <a
+            href="https://github.com/atbranco/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
